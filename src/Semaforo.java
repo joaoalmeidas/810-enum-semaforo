3950f91575fa236c6bb302f4772ab4fb080cf0bf
joao <joao182cx@gmail.com>
2018-12-24 20:02:46 -0200

    Criação da classe Semaforo.

--- src/Semaforo.java
+++ src/Semaforo.java
@@ -1,4 +1,18 @@
 
-public class Semaforo {
+public enum Semaforo {
+	
+	VERMELHO(20),
+	VERDE(20),
+	AMARELO(5);
+	
+	private final int duracao;
 
+	private Semaforo(int duracao) {
+		this.duracao = duracao;
+	}
+
+	public int getDuracao() {
+		return duracao;
+	}
+	
 }
